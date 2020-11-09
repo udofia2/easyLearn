60562ccadc83df00912f67d8bad823f56a3801bb
udofia2 <enalsy22@gmail.com>
2020-11-09 17:41:22 +0100

    database connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+require('./database')()
 
 const app = express()
+
+
 
 app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.host}:${process.env.PORT}`))
