ae8ac7a2531bb0fea64013e72b82f6a42a3dac86
udofia2 <enalsy22@gmail.com>
2020-11-09 17:37:30 +0100

    server setup

---
+++ server.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.host}:${process.env.PORT}`))
